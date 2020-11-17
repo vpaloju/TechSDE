408f9ad8ee035110dde72e89fed05dfffd8e9fe5
vpaloju <venkataramanchary.paloju@oracle.com>
2020-11-17 10:41:10 +0530

    Updated print patterns and print pascal Triangle

--- crackcode/src/main/java/interviewquestions/PrintStarPatterns.java
+++ crackcode/src/main/java/interviewquestions/PrintStarPatterns.java
@@ -2,51 +2,75 @@ package interviewquestions;
 
 public class PrintStarPatterns {
 
-    /**
-     *
-     * @param rows
-     */
-    public static void printHalfPyramid(int rows) {
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+  /**
+   * @param rows
+   */
+  public static void printHalfPyramid(int rows) {
+    for (int i = 1; i <= rows; i++) {
+      for (int j = 1; j <= i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
     }
+  }
 
-    /**
-     *
-     * @param rows
-     */
-    public static void printInvertedHalfPyramid(int rows) {
-        for (int i = rows; i >= 0; i--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+  /**
+   * @param rows
+   */
+  public static void printHalfPyramidNumbers(int rows) {
+    for (int i = 1; i <= rows; i++) {
+      for (int j = 1; j <= i; j++) {
+        System.out.print(j + " ");
+      }
+      System.out.println();
     }
+  }
 
-    /**
-     *
-     * @param rows
-     */
-    public static void printStairCase(int rows) {
-        for (int j = 0; j < rows; j++) {
-            for (int i = 1; i <= rows; i++) {
-                System.out.print(i < rows - j ? " " : "#");
-            }
-            System.out.println("");
-        }
+  /**
+   * @param rows
+   */
+  public static void printInvertedHalfPyramid(int rows) {
+    for (int i = rows; i >= 0; i--) {
+      for (int j = 1; j <= i; j++) {
+        System.out.print("* ");
+      }
+      System.out.println();
     }
+  }
 
-    public static void main(String[] args) {
-        printHalfPyramid(5);
-        System.out.println();
-        System.out.println("#################");
-        System.out.println();
-        printInvertedHalfPyramid(5);
-        printStairCase(5);
+  /**
+   * @param rows
+   */
+  public static void printStairCase(int rows) {
+    for (int j = 0; j < rows; j++) {
+      for (int i = 1; i <= rows; i++) {
+        System.out.print(i < rows - j ? " " : "#");
+      }
+      System.out.println("");
     }
+  }
+
+  public static void pyramidPattern(int rows) {
+    for (int i = 1; i <= rows; ++i) {
+      for (int space = 1; space <= rows - i; ++space) {
+        System.out.print("  ");
+      }
+      for (int k = 1; k < 2 * i; k++) {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+
+  public static void main(String[] args) {
+    int n = 5;
+    printHalfPyramid(n);
+    printHalfPyramidNumbers(n);
+    System.out.println();
+    System.out.println("#################");
+    System.out.println();
+    printInvertedHalfPyramid(n);
+    printStairCase(n);
+    pyramidPattern(n);
+  }
 }
--- crackcode/src/main/java/interviewquestions/serviceNow/PascalTriangle.java
+++ crackcode/src/main/java/interviewquestions/serviceNow/PascalTriangle.java
@@ -3,11 +3,29 @@ package interviewquestions.serviceNow;
 public class PascalTriangle {
 
   public static void printPascal(int n) {
-    for (int line = 1; line <= n; line++) {
+    for (int row = 1; row <= n; row++) {
       int c = 1; //Represents c(l,i).
-      for (int i = 1; i <= line; i++) {
+      for (int i = 1; i <= row; i++) {
         System.out.print(c + " ");
-        c = c * (line - i) / i;
+        c = c * (row - i) / i;
+      }
+      System.out.println();
+    }
+  }
+
+  public static void printPascalTriangle(int rows) {
+    int coef = 1;
+    for (int i = 0; i < rows; i++) {
+      for (int space = 1; space < rows - i; ++space) {
+        System.out.print("  ");
+      }
+
+      for (int j = 0; j <= i; j++) {
+        if (j == 0 || i == 0)
+          coef = 1;
+        else
+          coef = coef * (i - j + 1) / j;
+        System.out.printf("%4d", coef);
       }
       System.out.println();
     }
@@ -15,7 +33,6 @@ public class PascalTriangle {
 
   public static void main(String[] args) {
     printPascal(5);
-
-
+    printPascalTriangle(5);
   }
 }
