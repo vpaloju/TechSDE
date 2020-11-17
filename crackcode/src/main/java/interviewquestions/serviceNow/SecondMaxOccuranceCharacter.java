8fe696aeaa20f89a51dc99ff3596fa36282469b0
vpaloju <venkataramanchary.paloju@oracle.com>
2020-11-17 17:24:20 +0530

    Added new Interview Questions w.r.t Strings

---
+++ crackcode/src/main/java/interviewquestions/serviceNow/SecondMaxOccuranceCharacter.java
@@ -0,0 +1,30 @@
+package interviewquestions.serviceNow;
+
+public class SecondMaxOccuranceCharacter {
+  public static char secondMaxOccuranceChar(String str) {
+    int ASCII_SIZE = 256;
+    int[] map = new int[ASCII_SIZE];
+    char[] charArray = str.toLowerCase().toCharArray();
+    int max = 0, secondMax = 0;
+    char maxChar = '\n', secondMaxChar = '\n';
+    for (int i = 0; i < charArray.length; i++) {
+      map[charArray[i]]++;
+    }
+    for (int i = 0; i < ASCII_SIZE; i++) {
+      if (map[i] > max) {
+        secondMaxChar = maxChar;
+        max = map[i];
+        maxChar = (char) i;
+      } else if (map[i] > secondMax) {
+        secondMax = map[i];
+        secondMaxChar = (char) i;
+      }
+    }
+    return secondMaxChar;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(secondMaxOccuranceChar("servicenowproductdevelopment"));
+  }
+
+}
--- crackcode/src/main/java/interviewquestions/strings/PermutationsOfString.java
+++ crackcode/src/main/java/interviewquestions/strings/PermutationsOfString.java
@@ -2,43 +2,41 @@ package interviewquestions.strings;
 
 public class PermutationsOfString {
 
-    public static void permute(String str, int start, int end) {
-        if (start == end) {
-            System.out.println(str);
-        } else {
-            for (int i = start; i <= end; i++) {
-                str = swap(str, start, i);
-                permute(str, start + 1, end);
-                str = swap(str, start, i);
-            }
-        }
+  public static void permute(String str, int start, int end) {
+    if (start == end) {
+      System.out.println(str);
+    } else {
+      for (int i = start; i <= end; i++) {
+        str = swap(str, start, i);
+        permute(str, start + 1, end);
+        str = swap(str, start, i);
+      }
     }
+  }
 
-    public static String swap(String str, int i, int j) {
-        char[] charArray = str.toCharArray();
-        char temp = charArray[i];
-        charArray[i] = charArray[j];
-        charArray[j] = temp;
-        return String.valueOf(charArray);
+  public static String swap(String str, int i, int j) {
+    char[] charArray = str.toCharArray();
+    char temp = charArray[i];
+    charArray[i] = charArray[j];
+    charArray[j] = temp;
+    return String.valueOf(charArray);
+  }
+
+  public static void permutations(String input, String result) {
+    if (input.length() == 0) {
+      System.out.println(result + "");
+      return;
     }
-
-    public static void permutations(String input, String result) {
-        if (input.length() == 0) {
-            System.out.println(result + "");
-            return;
-        }
-        for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-            String subString = input.substring(0, i) + input.substring(i + 1);
-            permutations(subString, result + ch);
-
-        }
-
+    for (int i = 0; i < input.length(); i++) {
+      char ch = input.charAt(i);
+      String subString = input.substring(0, i) + input.substring(i + 1);
+      permutations(subString, result + ch);
     }
+  }
 
-    public static void main(String[] args) {
-        permute("abc",0,2);
-        permutations("abc","");
-    }
+  public static void main(String[] args) {
+    permute("abc", 0, 2);
+    permutations("abc", "");
+  }
 
 }
